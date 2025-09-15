29aa42f2bd6ac520310ee29b77dab68d5ef350b2
Nida Amir <142287805+Nidaamir083@users.noreply.github.com>
2025-09-15 10:10:35 +0500

    Update retrieval_module.py

--- retrieval_module.py
+++ retrieval_module.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 # ---------------------------
 # CONFIG
 # ---------------------------
-Entrez.email = "your-email@example.com"   # REQUIRED for PubMed
+Entrez.email = "nida.amir0083@gmail.com"   # REQUIRED for PubMed
 CURRENT_YEAR = datetime.now().year
 
 DISEASE_TERMS = [
